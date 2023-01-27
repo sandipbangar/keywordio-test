843d00c1637cc5b6a98cd2a8462b2e647d0553e0
Sandip Bangar <sandip.bangar1999@outlook.com>
2023-01-28 03:39:25 +0530

    Added Ad Insights header to table 2 and borders

--- src/components/DoughnutComp.jsx
+++ src/components/DoughnutComp.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ApexCharts from 'react-apexcharts';
+import { Typography } from '@mui/material';
+import "../styles/DoughnutStyle.css"
 
 class DoughnutComp extends Component {
   constructor(props) {
@@ -22,7 +24,13 @@ class DoughnutComp extends Component {
 
   render() {
     return (
-      <ApexCharts options={this.state.options} series={this.state.series} type="donut" width={600} />
+      <>
+        <div className="donut-container">
+          <Typography className="donut-ad-insights">Ad Insights</Typography>
+          <ApexCharts className="donut" options={this.state.options} series={this.state.series} type="donut" width={550} />
+        </div>
+      </>
+
     );
   }
 }
